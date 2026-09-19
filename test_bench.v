1a93a29993b1be8b93eb2fece0073a4ff2c2e3bb
Lingsan-nity <vu.ha2007@hcmut.edu.vn>
2026-09-19 22:57:26 +0700

    Update test_bench.v

--- test_bench.v
+++ test_bench.v
@@ -2,18 +2,18 @@
 
 module test_bench;
 
-    reg a;
-    reg b;
+    reg [3:0] a;
+    reg  [3:0] b;
 
     initial begin
         // ==================== CASE 1 ====================
         a = 4'b1001;
         b = 4'b0011;
-        $display("Bitwise XOR:  a=%b,b=%b", a,b,a ^ b);
-        $display("Bitwise And:  a=%b,b=%b" ,a,b,a & b);
-        $display("Bitwise Or:   a=%b,b=%b" ,a,b,a | b);
-        $display("Reduction And: a=%b,b=%b",&a);
-        &display("Reduction XOr: b=%b", ^a);
+        $display("Bitwise XOR:  a=%b,b=%b ->result=%b", a,b,a ^ b);
+        $display("Bitwise And:  a=%b,b=%b ->result=%b" ,a,b,a & b);
+        $display("Bitwise Or:   a=%b,b=%b -> result=%b" ,a,b,a | b);
+        $display("Reduction And: a=%b ->result=%b",&a);
+        $display("Reduction XOr: b=%b -> result=%b", ^a);
     end
 
 endmodule
